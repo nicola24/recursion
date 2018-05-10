a779cdd2000436f74544f4b927df4b6acd49c01d
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-05-09 19:04:01 -0700

    transfert old repo to new one

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var nodes = [];
+
+  var searchNodes = (node) => {
+    // compare node's classname with className
+    var parts = node.className.split(' ');
+    if (parts.indexOf(className) >= 0){
+      // if matched, save node
+      nodes.push(node);
+    }
+
+    // itereate over children
+    for (var i = 0; i < node.children.length; i++) {
+      // for each child, invoke searchNodes
+      searchNodes(node.children[i]);
+    }
+  };
+
+  searchNodes(document.body);
+
+  return nodes;
 };
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,30 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+	var result = [];
+
+	if (Array.isArray(obj)) {
+		_.forEach(obj, function(x) {
+			result.push(stringifyJSON(x));
+		});
+		return `[${result.join(',')}]`;
+	}
+
+  	if (obj === null) return 'null';
+	if (typeof obj === 'string') return `"${obj}"`;
+	if (typeof obj === 'number', 'boolean') return obj.toString();
+
 };
+
+
+// unpassed tests..
+
+// stringifyJSON({});                       // '{}'
+// stringifyJSON({'a': 'apple'});           // '{"a":"apple"}'
+// stringifyJSON({'foo': true, 'bar': false, 'baz': null});
+// stringifyJSON({'boolean, true': true, 'boolean, false': false, 'null': null });
+// stringifyJSON({'a': {'b': 'c'}});
+// stringifyJSON({'a': ['b', 'c']});
+// stringifyJSON([{'a': 'b'}, {'c': 'd'}]);
+// stringifyJSON({'a': [], 'c': {}, 'b': true});
